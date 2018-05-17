a1735b63c802e41fdd92a30e13e643535108c4a3
stephshea <stephcshea@gmail.com>
2018-05-17 14:33:29 -0400

    Completed Class Demo

---
+++ MVA_Class_Demo/DrinksMachine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVA_Class_Demo
+{
+    class DrinksMachine
+    {
+        //the following statements declare private member variables
+        public string _location;
+        public string _make;
+        public string _model;
+
+        //the following statements declare public methods
+
+        public void MakeCappuccino()
+        {
+            Console.WriteLine("Cappucino is made.");
+        }
+
+        public void MakeExpresso()
+        {
+            //Method logic goes here
+        }
+    }
+}
---
+++ MVA_Class_Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVA_Class_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrinksMachine myMachine = new DrinksMachine();
+            myMachine._location = "Kitchen";
+            myMachine._model = "DM1000";
+
+            Console.WriteLine(myMachine._location);
+
+            myMachine.MakeCappuccino();
+        }
+    }
+}
